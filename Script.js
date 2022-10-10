13a2f6620c92659e54e1d2e0e780817afc0f8a67
Kishan Singh <kishanrsingh123@gmail.com>
2022-10-10 14:18:48 +0530

    COMMIT ALL

---
+++ Script.js
@@ -0,0 +1,10 @@
+function myfunction(){
+   let a = document.getElementsByClassName('imagebar');
+
+   a.children[0].style.width="500px";
+   a.children[0].style.height="500px";
+   a.children[1].style.width="500px";
+   a.children[1].style.height="500px";
+   a.children[2].style.width="500px";
+   a.children[2].style.height="500px";
+} 
